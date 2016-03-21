c6630b95109aa68f3d7ffbc3a19d6bc964185b96
Adam Nguyen <AdamNgu@gmail.com>
2016-03-21 16:21:55 -0400

    Bare minimum working

--- frontend/buttons.jsx
+++ frontend/buttons.jsx
@@ -3,15 +3,16 @@ var React = require('react');
 var Buttons = React.createClass({
   render: function(){
     if(this.props.running){
-      btn1Text = "stop";
+      btn1Text = "Stop";
     }
     else {
-      btn1Text = "start";
+      btn1Text = "Start";
     }
 
     return(
       <div>
-        <button onClick={this.props.toggleRunning}/>
+        <button onClick={this.props.toggleRunning}>{btn1Text}</button>
+        <button onClick={this.props.reset}>Reset</button>
       </div>
     );
   }
--- frontend/pomodoro.jsx
+++ frontend/pomodoro.jsx
@@ -4,7 +4,13 @@ var React = require('react'),
 
 var Pomodoro = React.createClass({
   getInitialState: function(){
-      return {timeRemaining: 1500, running: false, isBreak: false};
+      return {
+        timeRemaining: 1500,
+        running: false,
+        isBreak: false,
+        workTime: 25,
+        breakTime: 5
+      };
   },
 
   componentDidMount: function(){
@@ -14,11 +20,31 @@ var Pomodoro = React.createClass({
   tick: function(){
     if(this.state.running){
       this.setState({timeRemaining: this.state.timeRemaining - 1});
+
+      // if the timer reaches 0, change isBreak status and reset timer
+      if(this.state.timeRemaining <= 0){
+        var timeRemaining;
+        if(this.state.isBreak){
+          timeRemaining = this.state.workTime * 60;
+        }
+        else {
+          timeRemaining = this.state.breakTime * 60;
+        }
+        this.setState({timeRemaining: timeRemaining, isBreak: !this.state.isBreak});
+      }
     }
   },
 
   toggleRunning: function(){
     this.setState({running: !this.state.running});
+  },
+
+  reset: function(){
+    this.setState({
+      timeRemaining: this.state.workTime * 60,
+      running: false,
+      isBreak: false
+    });
   },
 
   render: function(){
@@ -29,6 +55,7 @@ var Pomodoro = React.createClass({
         <Buttons
           running={this.state.running}
           toggleRunning={this.toggleRunning}
+          reset={this.reset}
         />
       </div>
     );
